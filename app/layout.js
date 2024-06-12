2052cf1d8649bd5009fb042ba436355903704c9f
FranckPedron <franckyp1+rapports_git@gmail.com>
2024-06-12 14:33:18 +0200

    Ajout de Rtk

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,21 @@
+'use client'
 import "./globals.css";
+import StoreProvider from "@/app/StoreProvider";
 
-export const metadata = {
-  title: "Gradient App",
-  description: "Generate linear or radial gradients",
-};
+
+// export const metadata = {
+//   title: "Gradient App",
+//   description: "Generate linear or radial gradients",
+// };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+
+        <html lang="en">
+          <body>
+          <StoreProvider>{children}</StoreProvider>
+          </body>
+        </html>
+
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,7 +1,20 @@
+'use client'
+
+import Gradient from "@/app/gradient/page";
+import Colors from "@/components/Colors";
+import DirectionButtons from "@/components/Buttons/DirectionButtons";
+import RandomButtons from "@/components/Buttons/RandomButtons";
+import {useAppSelector} from "@/lib/hooks";
 
 export default function Home() {
+    const type = useAppSelector((state) => state.type);
+
   return (
-    <>
-    </>
+  <div className="app">
+      <RandomButtons />
+        <Colors />
+        <Gradient />
+      {type === "linear" && <DirectionButtons/>}
+</div>
   );
 }
